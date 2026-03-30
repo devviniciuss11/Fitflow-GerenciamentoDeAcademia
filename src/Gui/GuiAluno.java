724e04c37d7af0fc0ee638f18d3ea4521285b450
Vinicius De Sousa <devvinicius11@gmail.com>
2026-03-30 11:41:43 -0300

    Update GuiAluno.java

--- src/Gui/GuiAluno.java
+++ src/Gui/GuiAluno.java
@@ -9,12 +9,13 @@ public class GuiAluno {
     public void menu(){
         int opc1 =1;
         while (opc1 ==1){
-            System.out.println("Menu Aluno");
-            System.out.println("1- Cadastrar Aluno");
-            System.out.println("2- Listar Alunos");
-            System.out.println("3- Remover Aluno");
-            System.out.println("4- Editar Aluno");
-            System.out.println("5- Sair");
+            System.out.println("           MENU ALUNO           ");
+            System.out.println(" [1] - Cadastrar Novo Aluno");
+            System.out.println(" [2] - Listar Alunos Cadastrados");
+            System.out.println(" [3] - Remover Aluno");
+            System.out.println(" [4] - Alterar Dados de um Aluno");
+            System.out.println(" [5] - Voltar ao Menu Principal");
+            System.out.print("Escolha uma opção: ");
             int op;
             try {
                 op =sc1.nextInt();
