4467c0ac4bac7b1efa2d3fdf74802ff237b9573a
Igor Gustavo <igor.gustavo.br007@gmail.com>
2026-04-25 19:08:23 -0300

    CrudFuncionario-V2.0

---
+++ src/Entidade/Endereco.java
@@ -0,0 +1,48 @@
+package Entidade;
+
+public class Endereco {
+    private String Cep;
+    private String Bairro;
+    private String NomeRua;
+    private String complemento;
+    private int NumCasa;
+
+    public Endereco(String cep, String bairro, String nomeRua, String complemento, int numCasa) {
+        Cep = cep;
+        Bairro = bairro;
+        NomeRua = nomeRua;
+        this.complemento = complemento;
+        NumCasa = numCasa;
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+    public void setCep(String cep) {
+        Cep = cep;
+    }
+    public String getBairro() {
+        return Bairro;
+    }
+    public void setBairro(String bairro) {
+        Bairro = bairro;
+    }
+    public String getNomeRua() {
+        return NomeRua;
+    }
+    public void setNomeRua(String nomeRua) {
+        NomeRua = nomeRua;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public int getNumCasa() {
+        return NumCasa;
+    }
+    public void setNumCasa(int numCasa) {
+        NumCasa = numCasa;
+    }
+}
